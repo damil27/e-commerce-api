56c3458c7aeefd158047b03278d56b224843794d
damin <34189426+damil27@users.noreply.github.com>
2025-04-02 20:00:56 +0100

    resolved circular relationship

--- store/models.py
+++ store/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    feature_products = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal=2)
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
 
-class Customer(models.Model):
+
+class Customer(models.Model):   
     MEMBERSHIP_PREMIUM = "pr"
     MEMBERSHIP_STANDARD = "st"
     MEMBERSHIP_FREE = "fr"
@@ -22,7 +32,9 @@ class Customer(models.Model):
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
-    membership = models.CharField(  max_length=2, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_FREE)
+    membership = models.CharField(
+        max_length=2, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_FREE)
+
 
 class Order(models.Model):
     STATUS_PENDING = 'p'
@@ -34,4 +46,30 @@ class Order(models.Model):
         (STATUS_FAILED, 'Returned'),
     ]
     place_at = models.DateTimeField(auto_now_add=True)
-    PAYMENT_STATUS  = models.CharField(max_length=1, choices=STATUS_CHOICES, default=STATUS_PENDING)
+    PAYMENT_STATUS = models.CharField(
+        max_length=1, choices=STATUS_CHOICES, default=STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItems(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    Customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE)
+
+
+class Cart(models.Model):
+    created_at = models.DateField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
